dbaaac82e6712fdfd03ef09b38f2bfb87d629b4f
huayikok <huayi.kok@gmail.com>
2023-06-08 00:54:16 +0800

    Commit
    - Add drawer mini component.
    - Write setup readme.

--- SETUP_README.md
+++ SETUP_README.md
@@ -1,2 +1,28 @@
-# NODE VERSION
+# current Node version
 14.15.1
+
+# install NVM to handle different Node versions
+
+# ----------------------------------------------
+
+# 👇️ create a React project setup
+npx create-react-app khymyprofolio
+
+# 👇️ with NPM
+npm install @material-ui/core
+npm install @material-ui/icons
+npm install @mui/material @emotion/react @emotion/styled --force
+
+# 👇️ only if you use @mui/icons-material
+npm install @mui/icons-material --force
+
+# 👇️ only if you use @mui/lab
+npm install @mui/lab --force
+
+# ----------------------------------------------
+
+# 👇️ local run your project
+npm run start
+
+# 👇️ build your project
+npm run build
--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import MiniDrawer from './ui-component/drawer/component_drawer_mini';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MiniDrawer />
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
